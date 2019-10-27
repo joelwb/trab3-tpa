9aebe2444e9c9bc316029f69d8919fb7c8e71dce
André Martins <objetovazio@gmail.com>
2019-10-27 10:46:30 -0300

    comments added
    Results not passing. shold try in C++

--- andre/uva11340.py
+++ andre/uva11340.py
@@ -1,24 +1,38 @@
-# uva11340
+"""
+Author: André Martins
+UVa Online Judge Problem: 11340 Newspaper
 
+This code calculates the total value of an inputed article
+according with the value of each charactere and show
+
+"""
 
 def main():
+
+    # Get the total of case tests
     numberTest = int(input())
 
     for i in range(numberTest):
+
+        # Get the number of chars that should be inputed
         numberPaidChar = int(input())
 
+        # Fill the dictionary according with {'char': 'paid value'}
         paidChars = {}
         for j in range(numberPaidChar):
             inputCharVal = input().split(' ')
             paidChars[inputCharVal[0]] = int(inputCharVal[1])
         # end-for
 
+        # Get the number of lines of the article, read and concatenate
         article = ""
         numberLinesArticle = int(input())
         for j in range(numberLinesArticle):
             article = article + input()
         # end-for
 
+        # Iterate on the chars of the article and get its total occurrence
+        # taking care to dont reapeat the calculation
         calculatedChar = {}
         for char in article:
             if(char not in calculatedChar.keys()):
@@ -27,6 +41,7 @@ def main():
             # end-if
         # end
 
+        # iterate on the amount of chars and multiply by the paid value for each
         totalValue = 0
         for char in paidChars.keys():
             if(char in calculatedChar.keys()):
@@ -34,12 +49,12 @@ def main():
             #end-if
         #end-for
 
+        # print the result
         finalValue = totalValue / 100.0
         print('%.2f$' % (finalValue))
     # end-for
 
 # end-main()
-
 
 if __name__ == "__main__":
     main()
--- andre/uva12247.py
+++ andre/uva12247.py
@@ -1,10 +1,27 @@
+"""
+Author: André Martins
+UVa Online Judge Problem: 12247 Jollo
+
+This code verify which is the last card that the prince needs 
+to receive from the servant and win on Jollo from his sister.
+
+"""
+
+"""
+This method get the input cards, verifying if receveid any 0 on input, to stop the code.
+Also, it cleans and update the array of valid cards on the deck.
+"""
 def inputCards():
+    #Create array of input cards and Array of Valid cards
     cards: list = []
     validCards: list = [True for x in range(53)]
 
+    # Set the 0 Inputs to false and read the keyboard input
     hasZeroInput: bool = False
     cards = input().split(' ')
     
+    # Iterate on the input cards, converting to integer 
+    # and verying zeros. Also update the array of valid cards
     for i in range(len(cards)):
         toInt = int(cards[i])
         cards[i] = toInt
@@ -26,14 +43,19 @@ def main():
     princeCards:list = []
     
     while(not hasZeroInput):
+        # Separate de Cards
         princessCards = cards[0:3]
         princeCards = cards[3:5]
 
+        # Sort the cards from minor to maior
         princessCards.sort()
         princeCards.sort()
         
+        # Set the Default value when the prince lose.
         newCard = -1
 
+        # Verify if the first prince's card is bigger than the last princess card
+        # This means that the prince already has 2 bigger cards
         if(princeCards[0] > princessCards[2]):
             for i in range(1, 52):
                 if(validCards[i]):
@@ -43,6 +65,8 @@ def main():
             #end-for
         #end-if
         else:
+            # Verify if the second prince's card is bigger than the last Princess Card
+            # This means that the prince still have a chance if theres any card bigger than princess[2]
             if(princeCards[1] > princessCards[2]):
                 for i in range(princessCards[2] + 1, 52):
                     if(validCards[i]):
@@ -52,6 +76,8 @@ def main():
                 #end-for
             #end-if
             
+            # Verify if the first prince's card is bigger than the second Princess Card
+            # This means that the prince still have a chance if theres any card bigger than princess[1]
             if(princeCards[0] > princessCards[1]):
                 for i in range(princessCards[1], 52):
                     if(validCards[i]):
