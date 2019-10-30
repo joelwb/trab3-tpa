994d31a9a617a6486fd9996ba191b674426417f8
joelwb <joel-willb@hotmail.com>
2019-10-30 23:40:46 +0000

    Exercício 10038 Jolly Jumpers

---
+++ joel/10038.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main(){
+    int n;
+
+    while (cin >> n){
+        int seq[n];
+        int diffSeq[n-1] = {0};
+
+        for (int i = 0; i < n; i++){
+            cin >> seq[i];
+        }
+
+        bool flag = false;
+
+        for (int i = 1; i < n; i++){
+            int diff = abs(seq[i] - seq[i-1]);
+            if (diff > n-1) {
+                flag = true;
+                break;
+            }
+
+            diffSeq[diff-1] = 1;
+        }
+
+        if (!flag) {
+            for (int i = 0; i < n-1; i++){
+                if (diffSeq[i] != 1) {
+                    flag = true;
+                    break;
+                }
+            }
+        }
+
+        if (flag) {
+            cout << "Not jolly" << endl;
+        } else {
+            cout << "Jolly" << endl;
+        }
+    }
+}
