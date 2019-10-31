16bbafeafd94e2316d8d7b481bab613f29e69c79
joelwb <joel-willb@hotmail.com>
2019-10-31 00:13:23 +0000

    Exercicio 10107 What is the Median?

---
+++ joel/10107.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<int> X;
+    int x;
+    while (cin >> x) {
+        int pos = 0;
+        for (int i = 0; i < X.size(); i++){
+            if (X[i] > x) {
+                pos = i;
+                break;
+            }
+        }
+
+        X.insert(X.begin() + pos, x);
+        int meio = X.size() / 2;
+        if (X.size() % 2 != 0) {
+            cout << X[meio] << endl;
+        } else {
+            int median = (X[meio] + X[meio - 1]) / 2;
+            cout << median << endl;
+        }
+    }
+}
