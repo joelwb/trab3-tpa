6f49d967f9021dda088415506ff69a0036668735
NicolasSampaio <nicolas.pereira.sampaio16@gmail.com>
2019-11-03 18:54:06 -0300

    Accepted

---
+++ nicolas/514/514.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    while (true)
+    {
+        int qntVagao, valor;
+        stack<int> pilha;
+        cin >> qntVagao;
+        if (qntVagao == 0)
+            break;
+        while (true)
+        {
+            while (!pilha.empty())
+            {
+                pilha.pop();
+            }
+            int comp = 0;
+            for (int i = 0; i < qntVagao; i++)
+            {
+                cin >> valor;
+                if (valor == 0)
+                    break;
+
+                while (comp < qntVagao && comp != valor)
+                {
+                    if (!pilha.empty() && pilha.top() == valor)
+                    {
+                        break;
+                    }
+                    comp++;
+                    pilha.push(comp);
+                }
+                if (pilha.top() == valor)
+                {
+                    pilha.pop();
+                }
+            }
+            if (valor == 0)
+            {
+                printf("\n");
+                break;
+            }
+            if (pilha.empty())
+            {
+                cout << "Yes" << endl;
+            }
+            else
+            {
+                cout << "No" << endl;
+            }
+        }
+    }
+}
